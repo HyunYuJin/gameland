31ebec98ffac4b105d54d3d36d4f3d331f87bf30
HyeonYuJin <gusdbwls9772@gmail.com>
2023-01-12 01:11:03 +0900

    style: 💅styled component 추가

--- mbit/src/components/intro.tsx
+++ mbit/src/components/intro.tsx
@@ -1,17 +1,80 @@
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import mbit from '../assets/images/mbit.png'
 
 function Intro () {
   return (
-    <div className="intro">
-      <h1 className="logo">나의 MBIT</h1>
-      <p>My Best IT personalities</p>
-      <em>" 나의 개발 유형은?! "</em>
+    <Wrapper>
+      <Logo>나의 MBIT</Logo>
+      <Paragraph1>My Best IT personalities</Paragraph1>
+      <Paragraph2>“ 나의 개발 유형은⁈ ”</Paragraph2>
 
-      <img src={mbit} alt="mbit" />
-      <Link to="/questions/1" className="start">시작하기</Link>
-    </div>
+      <Image src={mbit} alt="mbit" />
+      
+      <Button to="/questions/1" className="start">시작하기</Button>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  min-width: 360px;
+  margin: 0 auto;
+  padding: 45px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  white-space: nowrap;
+`
+
+const Logo = styled.h1`
+  font-family: 'GmarketSansMedium';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 45px;
+  line-height: 45px;
+  color: #000000;
+`
+
+const Paragraph1 = styled.p`
+  padding-top: 10px;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  color: var(--primary);
+`
+
+const Paragraph2 = styled.em`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 23px;
+  padding-top: 36px;
+  color: #000000;
+`
+
+const Image = styled.img`
+  padding-top: 36px;
+  width: 100%;
+  height: 100%;
+`
+
+const Button = styled(Link)`
+  margin-top: 36px;
+  max-width: 300px;
+  width: 100%;
+  height: 52px;
+  background-color: #FFFFFF;
+  color: var(--primary);
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
+  border: 1.5px solid #191919;
+  border-radius: 10px;
+  font-weight: 900;
+  font-size: 16px;
+  line-height: 23px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 export default Intro
