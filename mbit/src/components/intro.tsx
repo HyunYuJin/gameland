76d20bf8b89d44f3887a54d988e1cc96f25059fd
HyeonYuJin <gusdbwls9772@gmail.com>
2023-01-27 00:14:43 +0900

    :lipstick: 디자인 전체 수정중
    예쁘게 색상 변경 및 버튼 디자인 등..

--- mbit/src/components/intro.tsx
+++ mbit/src/components/intro.tsx
@@ -5,19 +5,19 @@ import mbit from '../assets/images/mbit.png'
 function Intro () {
   return (
     <Wrapper>
-      <Logo>나의 MBIT</Logo>
+      <Image src={mbit} alt="mbit" />
+
       <Paragraph1>My Best IT personalities</Paragraph1>
       <Paragraph2>“ 나의 개발 유형은⁈ ”</Paragraph2>
-
-      <Image src={mbit} alt="mbit" />
+      <Logo>My MBIT</Logo>
       
-      <Button to="/questions" className="start">시작하기</Button>
+      <Button to="/questions" className="start">Start!</Button>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  max-width: 300px;
+  max-width: 400px;
   min-width: 280px;
   margin: 0 auto;
   padding: 45px;
@@ -27,29 +27,29 @@ const Wrapper = styled.div`
   white-space: nowrap;
 `
 
-const Logo = styled.h1`
-  font-family: 'GmarketSansMedium';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 45px;
-  line-height: 45px;
-  color: #000000;
-`
-
 const Paragraph1 = styled.p`
-  padding-top: 10px;
+  padding-top: 15px;
   font-weight: 500;
-  font-size: 14px;
+  font-size: 16px;
   line-height: 20px;
   color: var(--primary);
 `
 
 const Paragraph2 = styled.em`
+  padding-top: 6px;
   font-style: normal;
   font-weight: 700;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 23px;
-  padding-top: 36px;
+  color: #000000;
+`
+
+const Logo = styled.h1`
+  padding-top: 24px;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 45px;
   color: #000000;
 `
 
@@ -63,13 +63,13 @@ const Button = styled(Link)`
   margin-top: 36px;
   max-width: 300px;
   width: 100%;
-  height: 52px;
-  background-color: #FFFFFF;
-  color: var(--primary);
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
-  border: 1.5px solid #191919;
-  border-radius: 10px;
-  font-weight: 900;
+  height: 40px;
+  background-color: var(--primary);
+  color: var(--light);
+  box-shadow: var(--box-shadow);
+  border: 1px solid var(--primary);
+  border-radius: 24px;
+  font-weight: 400;
   font-size: 16px;
   line-height: 23px;
   display: flex;
--- mbit/src/components/question.tsx
+++ mbit/src/components/question.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, ChangeEvent } from 'react'
 import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { QuestionType, SelectType, AnswerType } from '../types'
 import { classes } from '@gameland/shared'
 import data from '../datas/data.json'
@@ -85,18 +85,19 @@ function Questions () {
   return (
     <Wrapper>
       <Status>
-        <Current>{params} / 10</Current>
+        <Current>{params} of 10</Current>
         <Progress status={params}></Progress>
       </Status>
 
       <Form onSubmit={onSubmit} action="" id="question-form" method="post">
         {mbits && mbits.map(mbit => (
           <Question key={mbit.pk} className={classes({current: mbit.pk === params})} data-index={mbit.pk}>
-            <Pk>Q{mbit.pk}. {mbit.content}</Pk>
+            <Pk>Question{mbit.pk}</Pk>
+            <Inquiry>{mbit.content}</Inquiry>
             <Answers>
               {mbit.answers && mbit.answers.map((answer, index) => (
                 <Answer key={answer.content}>
-                  <input type="radio" id={`answer-${answer.pk}`} name={`question-${mbit.pk}`} value={answer.developer} onChange={($event) => onChange($event, index)} />
+                  <RadioButton type="radio" id={`answer-${answer.pk}`} name={`question-${mbit.pk}`} value={answer.developer} onChange={($event) => onChange($event, index)} />
                   <Content htmlFor={`answer-${answer.pk}`} pk={index + 1}>{answer.content}</Content>
                 </Answer>
               ))}
@@ -105,8 +106,8 @@ function Questions () {
         ))}
 
         <Buttons>
-          {params !== 1 && <Prev type="button" onClick={onDecrease}>이전</Prev>}
-          {params !== mbits.length ? <Next type="button" onClick={onIncrease}>다음</Next>
+          {params !== 1 && <Prev type="button" onClick={onDecrease}>Prev</Prev>}
+          {params !== mbits.length ? <Next type="button" onClick={onIncrease}>Next</Next>
           : <Next type="submit">검사 결과</Next>}
         </Buttons>
       </Form>
@@ -114,32 +115,46 @@ function Questions () {
   )
 }
 
+const fadeInCheckbox = keyframes` 
+  from {
+    opacity: 0;
+    transform: rotateZ(-20deg);
+  }
+
+  to {
+    opacity: 1;
+    transform: rotateZ(0deg);
+  }
+`
+
 const Wrapper = styled.div`
-  max-width: 300px;
+  max-width: 400px;
   min-width: 280px;
   margin: 0 auto;
   padding: 0 45px;
+  color: var(--dark);
 `
 
 const Status = styled.div`
-  margin-bottom: 40px;
+  margin-bottom: 20px;
 `
 
 const Current = styled.span`
-  font-weight: 400;
-  font-size: 18px;
+  font-weight: 500;
+  font-size: 12px;
   line-height: 26px;
   display: flex;
   align-items: center;
   justify-content: center;
   margin-bottom: 12px;
-  color: var(--primary);
 `
 
 const Progress = styled.div<{status: number}>`
   width: 100%;
-  height: 6px;
-  background-color: #FFF;
+  height: 4px;
+  background-color: rgba(0, 0, 0, .07);
+  border-radius: 24px;
+  overflow: hidden;
 
   &::before {
     content: '';
@@ -162,34 +177,77 @@ const Question = styled.div`
 `
 
 const Pk = styled.h3`
-  font-weight: 900;
+  font-weight: 700;
   font-size: 18px;
   line-height: 26px;
   display: flex;
   align-items: center;
-  color: #000000;
+`
+
+const Inquiry = styled.p`
+  font-weight: 500;
+  padding-top: 8px;
+  font-size: 14px;
 `
 
 const Answers = styled.ol`
   display: flex;
   flex-direction: column;
   gap: 12px;
-  padding-top: 30px;
+  padding-top: 20px;
 `
 
-const Answer = styled.li``
+const Answer = styled.li`
+  padding: 6px 12px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 2px solid transparent;
+  border-radius: 10px;
+  background-color: #FFF;
+  color: #333333;
+  box-shadow: 0 4px 25px 0 rgba(0, 0, 0, 0.1);
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+  &:has(input[type="radio"]:checked) {
+    border: 2px solid var(--primary);
+    transition: border 0.3s;
+
+    &::after {
+      content: '';
+      position: absolute;
+      right: 5px;
+      top: 5px;
+      width: 16px;
+      height: 16px;
+      background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='UTF-8'%3F%3E%3Csvg width='32px' height='32px' stroke-width='1.74' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' color='%23719ec9'%3E%3Cpath d='M7 12.5l3 3 7-7' stroke='%23719ec9' stroke-width='1.74' stroke-linecap='round' stroke-linejoin='round'%3E%3C/path%3E%3Cpath d='M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z' stroke='%23719ec9' stroke-width='1.74' stroke-linecap='round' stroke-linejoin='round'%3E%3C/path%3E%3C/svg%3E");
+      background-size: contain;
+      animation: ${fadeInCheckbox} 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+    }
+  }
+`
 
 const Content = styled.label<{pk: number}>`
   font-style: normal;
   font-weight: 400;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 23px;
-  color: #000000;
-  
+  width: 100%;
+  cursor: pointer;
+  padding: 6px 10px 0 0;
+
   &::before {
-    content: '${props => props.pk}. ';
-    padding-left: 8px;
+    /* content: '${props => props.pk}. '; */
   }
+`
+
+const RadioButton = styled.input`
+  display: none;
 `
 
 const Buttons = styled.div`
@@ -203,11 +261,11 @@ const Buttons = styled.div`
 const Prev = styled.button`
   flex-basis: 100px;
   height: 40px;
-  background-color: #FFFFFF;
-  color: var(--primary);
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
-  border: 1.5px solid #191919;
-  border-radius: 10px;
+  background-color: var(--primary);
+  color: var(--light);
+  box-shadow: var(--box-shadow);
+  border: 1px solid var(--primary);
+  border-radius: 24px;
   font-weight: 900;
   font-size: 16px;
   line-height: 23px;
@@ -220,11 +278,11 @@ const Prev = styled.button`
 const Next = styled.button`
   flex-basis: 100px;
   height: 40px;
-  background-color: #FFFFFF;
-  color: var(--primary);
-  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
-  border: 1.5px solid #191919;
-  border-radius: 10px;
+  background-color: var(--primary);
+  color: var(--light);
+  box-shadow: var(--box-shadow);
+  border: 1px solid var(--primary);
+  border-radius: 24px;
   font-weight: 900;
   font-size: 16px;
   line-height: 23px;
